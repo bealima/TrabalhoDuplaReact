3eae802eee0d01fb9db77f2ad10fcbcf2db7ab5f
samuel eduardo <samueleduardo11@hotmail.com>
2021-11-13 15:40:49 -0400

    implementando css

--- src/Components/BotaoCadastrar.js
+++ src/Components/BotaoCadastrar.js
@@ -1,7 +1,7 @@
 function BotaoCadastrar(){
   return(
     <div>
-
+    
     </div>
   );
 }
--- src/Components/Cadastro.js
+++ src/Components/Cadastro.js
@@ -1,38 +1,103 @@
-import {useState} from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
+import ListaUsuarios from './ListaUsuarios';
+import { useState } from 'react';
 
-function Cadastro(){
-  function Cadastrar(){
-    const[tipoUsuario, setTipoUsuario] = useState();
-    const[nome, setNome] = useState();
-  }
-  return(
+import styles from '../Components/Cadastro.module.css';
+
+
+
+
+
+// function useFormik({
+//   initialValues
+// }){
+
+//   console.log(initialValues);
+//   return{
+//     values: initialValues
+//   }
+// }
+
+function Cadastro() {
+  const formik = useFormik({
+    initialValues: {
+      id: 0,
+      tipoUsuario: '',
+      nome: '',
+      dataNasc: '',
+      email: '',
+      senha: ''
+    },
+    onSubmit: values => {
+      console.log(values)
+    }
+  });
+  // function Cadastrar(e){
+  //   e.preventDefault()
+  //   let usuario= {id, tipoUsuario, nome, dataNasc, email, senha}
+  //   setUsuariosCadastrados([...usuariosCadastrados, usuario])
+  //   setId(id+1)
+
+  //}
+  // const[tipoUsuario, setTipoUsuario] = useState([]);
+  // const[nome, setNome] = useState('');
+  // const[dataNasc, setDataNasc] = useState('');
+  // const[email, setEmail] = useState('');
+  // const[senha, setSenha] = useState('');
+  // const[id, setId] = useState(0);
+
+
+  const [usuariosCadastrados, setUsuariosCadastrados] = useState([])
+  const [id, setId] = useState([])
+
+
+
+  return (
     <div>
-      <form>
+      <h1 className={styles.h1Title}>Cadastro de Usuario</h1>
+      <form onSubmit={formik.handleSubmit} className={styles.formElements}>
+
         <div>
           <label htmlFor="tipoUsuario">Tipo de usuário</label>
-          <input type="text"/>
+          <input type="text" id="tipoUsuario" name="tipoUsuario" onChange={formik.handleChange} value={formik.values.tipoUsuario} />
         </div>
+
         <div>
           <label htmlFor="nome">Nome completo*</label>
-          <input type="text"/>
+          <input type="text" id="nome" name="nome" onChange={formik.handleChange} value={formik.values.nome} />
         </div>
+
         <div>
           <label htmlFor="dataNascimento">Data de Nascimento*:</label>
-          <input type="date"/>
+          <input type="date" id="dataNasc" name="dataNasc" onChange={formik.handleChange} value={formik.values.dataNasc} />
         </div>
         <div>
           <label htmlFor="email">E-mail*:</label>
-          <input type="email" placeholder="E-mail" />
+          <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
         </div>
+
+        <div>
+          <label htmlFor="telefone">Telefone:</label>
+          <input type="tel" id="telefone" name="telefone" onChange={formik.handleChange} value={formik.values.telefone} />
+        </div>
+
         <div>
           <label htmlFor="senha">Senha*:</label>
-          <input type="password"/>
+          <input type="password" id="senha" name="senha" onChange={formik.handleChange} value={formik.values.senha} />
         </div>
+
         <div>
           <input type="checkbox" />
           <label htmlFor="primeiroEmprego">Primeiro Emprego?</label>
         </div>
+
+        <div>
+          <button type="submit">Cadastrar</button>
+        </div>
+
       </form>
+      <ListaUsuarios usuarios={usuariosCadastrados} setUsuarios={setUsuariosCadastrados} />
     </div>
   );
 }
